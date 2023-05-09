055a66eccef553e8bd37daac4ad750751dac1176
PaintableToast <69373789+PaintableToast@users.noreply.github.com>
2023-05-09 18:03:37 +0200

    Delete src/main/java/app/week2 directory

--- src/main/java/app/week2/Fractional.java
+++
@@ -1,7 +0,0 @@
-package app.week2;// This class was created by PaintableToast
-
-public class Fractional {
-    public static void main(final String args) {
-        System.out.println("Hello world");
-    }
-}
