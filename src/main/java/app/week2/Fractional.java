78cdf5753f956b4fd87e06efdd78dfb148d83a19
PaintableToast <rckimyon@yahoo.com>
2023-05-09 15:59:42 +0200

    hallo

---
+++ src/main/java/app/week2/Fractional.java
@@ -0,0 +1,7 @@
+package app.week2;// This class was created by PaintableToast
+
+public class Fractional {
+    public static void main(final String args) {
+        System.out.println("Hello world");
+    }
+}
